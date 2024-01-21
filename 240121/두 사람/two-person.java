1326b2b376533bb0a028178075f2f7fc8d267415
cjm40302537 <102509077+cjm40302537@users.noreply.github.com>
2024-01-21 23:41:56 +0900

    Title: 두 사람, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240121/두 사람/two-person.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter(" ");
+        int a= sc.nextInt();
+        char c =sc.next().charAt(0);
+
+        if(a>=19 && c=='M'){
+            System.out.println("1");
+        }else{
+           System.out.println("0"); 
+        }
+    }
+}
