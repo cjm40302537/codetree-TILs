1442532d27252a7c48da5dd2873b71a32f72539f
cjm40302537 <102509077+cjm40302537@users.noreply.github.com>
2024-02-02 13:49:10 +0900

    Title: 입력받은 수의 합과 평균, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240202/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int sum=0;
+        int cnt=0;
+        int a =sc.nextInt();
+        for(int i =1;i<=a;i++){
+            int n =sc.nextInt();
+            sum+=n;
+            cnt++;
+        }
+        double avg=(double)sum/cnt;
+        System.out.printf("%d %.1f",sum,avg);
+    }
+}
