30bcbac397e198e47a3c8ca7b2fe6ceddbd1f333
cjm40302537 <102509077+cjm40302537@users.noreply.github.com>
2024-02-02 21:08:35 +0900

    Title: 3n + 1 수열, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240202/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        int cnt=0;
+        for(;;){
+            if(n==1){
+                System.out.print(cnt);
+                break;
+            }
+            if(n%2==0){
+                n/=2;
+                cnt++;
+            }else{
+                n=3*n+1;
+                cnt++;
+            }
+
+        }
+    }
+}
