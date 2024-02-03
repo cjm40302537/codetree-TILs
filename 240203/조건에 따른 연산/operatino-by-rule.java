c374e263878bd81791b296db8e3be964ab678653
cjm40302537 <102509077+cjm40302537@users.noreply.github.com>
2024-02-03 11:03:50 +0900

    Title: 조건에 따른 연산, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240203/조건에 따른 연산/operatino-by-rule.java
+++ 240203/조건에 따른 연산/operatino-by-rule.java
@@ -4,13 +4,11 @@ public class Main {
         Scanner sc =new Scanner(System.in);
         int n =sc.nextInt();
         int cnt=0;
-        int sum=0;
         for(;;){
             if(n%2==0){
                 n=n*3+1;
                 cnt++;
-            }
-            if(n%2==1){
+            }else if(n%2==1){
                 n=n*2+2;
                 cnt++;
             }
