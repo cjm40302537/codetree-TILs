f7aaacc3a21c299b8a392be7922c4192013f80b7
cjm40302537 <102509077+cjm40302537@users.noreply.github.com>
2024-02-03 11:03:08 +0900

    Title: 조건에 따른 연산, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240203/조건에 따른 연산/operatino-by-rule.java
+++ 240203/조건에 따른 연산/operatino-by-rule.java
@@ -8,15 +8,13 @@ public class Main {
         for(;;){
             if(n%2==0){
                 n=n*3+1;
-                sum+=n;
                 cnt++;
             }
             if(n%2==1){
                 n=n*2+2;
-                sum+=n;
                 cnt++;
             }
-            if(sum>=1000){
+            if(n>=1000){
                 break;
             }
         }
